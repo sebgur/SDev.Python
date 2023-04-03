0f00e715eab2d09d496790bea56736d918d965ba
sebgur <sebgur@gmail.com>
2023-04-03 22:55:58 +0800

    adding FlooredExponentialDecay

---
+++ python/machinelearning/FlooredExponentialDecay.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+
+# Custom learning rate scheduler, exponentially decreases between given values
+class FlooredExponentialDecay(tf.keras.optimizers.schedules.LearningRateSchedule):
+    """ Custom learning rate scheduler, exponentially decreases between given values """
+    def __init__(self, initial_lr=1e-1, final_lr=1e-4, decay=0.96, decay_steps=100):
+        self.initial_lr = initial_lr
+        self.final_lr = final_lr
+        self.decay = decay
+        self.decay_steps = decay_steps
+
+    def __call__(self, step):
+        ratio = tf.cast(step / self.decay_steps, tf.float32)
+        coeff = tf.pow(self.decay, ratio)
+        return self.initial_lr * coeff + self.final_lr * (1.0 - coeff)
+
+    def get_config(self):
+        config = { 'initial_lr': self.initial_lr,
+                   'final_lr': self.final_lr,
+                   'decay': self.decay,
+                   'decay_steps': self.decay_steps }
+        return config
--- python/projects/xsabr_fit/xsabr_fit.py
+++ python/projects/xsabr_fit/xsabr_fit.py
@@ -2,9 +2,14 @@ import settings
 import os
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 from HaganSabrGenerator import HaganSabrGenerator, ShiftedHaganSabrGenerator
+from machinelearning.topology import compose_model
+from machinelearning.LearningModel import LearningModel
+from machinelearning.FlooredExponentialDecay import FlooredExponentialDecay
 
 # ################ ToDo ###################################################################################
+# Refresh marchine learning py
 # Implement generic training
 # Display result and metrics
 
@@ -25,10 +30,41 @@ else:
 data_file = os.path.join(data_folder, model_type + "_samples.tsv")
 print("Reading data from file: " + data_file)
 x_set, y_set, data_df = generator.retrieve_datasets(data_file)
+input_dim = x_set.shape[1]
+output_dim = y_set.shape[1]
+print("Input dimension: " + str(input_dim))
+print("Output dimension: " + str(output_dim))
 print(data_df.head())
 
-# Training
+# Initialize the model
+hidden_layers = ['softplus', 'softplus', 'softplus']
+num_neurons = 16
+dropout = 0.00
+keras_model = compose_model(input_dim, output_dim, hidden_layers, num_neurons, dropout)
 
+# Learning rate scheduler
+init_lr = 1e-1
+final_lr = 1e-4
+decay = 0.97
+steps = 100
+lr_schedule = FlooredExponentialDecay(init_lr, final_lr, decay, steps)
+
+# Optimizer
+optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
+# Display optimizer fields
+print("Optimizer settings")
+optim_fields = optimizer.get_config()
+for field in optim_fields:
+    print(field, ":", optim_fields[field])
+
+# Compile
+keras_model.compile(loss='mse', optimizer=optimizer)
+model = LearningModel(keras_model)
+
+# Train the network
+epochs = 1000
+batch_size = 1000
+history = model.train(x_set, y_set, epochs, batch_size)
 
 # print(x_set)
 # print(y_set.shape)
