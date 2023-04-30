2a4843b566c2053f7784bc5144aa673b49a8551b
sebgur <sebgur@gmail.com>
2023-04-30 23:02:10 +0800

    saving preliminary work on model save to file

--- python/projects/xsabr/xsabrfit.py
+++ python/projects/xsabr/xsabrfit.py
@@ -2,6 +2,7 @@
     vs prices/implied vols (outputs) are generated (or read from tsv) to train a network that
     learns the so-colled 'direct' calculation, i.e. prices from parameter. """
 import os
+from datetime import datetime
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
@@ -10,7 +11,7 @@ import settings
 from machinelearning.topology import compose_model
 from machinelearning.learningmodel import LearningModel
 from machinelearning.learningschedules import FlooredExponentialDecay
-from machinelearning.callbacks import SDevPyCallback, RefCallback
+from machinelearning.callbacks import RefCallback  #, SDevPyCallback
 from machinelearning.datasets import prepare_sets
 from tools.filemanager import check_directory
 from tools.timer import Stopwatch
@@ -20,30 +21,42 @@ from projects.xsabr import xsabrplot as xplt
 # ToDo: Export trained model to file
 # ToDo: Optional reading of model from file
 # ToDo: Visual comparison on shifted BS vols for more familiar demo
-# ToDo: Finalize and fine-train models on extended parameter range
 # ToDo: Put SABR and FB-SABR MC into analytics\fbsabr.py
 # ToDo: Import/translate Kienitz's PDEs from C#, especially if we have ZABR?
+# ToDo: Finalize and fine-train models on extended parameter range
 # ToDo: Store models in git and data in Kaggle
 
 # ################ Runtime configuration ##########################################################
 # MODEL_TYPE = "SABR"
 MODEL_TYPE = "ShiftedSABR"
-GENERATE_SAMPLES = False
-NUM_SAMPLES = 100 * 1000
-TRAIN_PERCENT = 0.90
-TRAIN = True
-EPOCHS = 100
-BATCH_SIZE = 1000
-SHOW_VOL_CHARTS = False
+GENERATE_SAMPLES = False  # If false, read dataset from file
+NUM_SAMPLES = 100 * 1000  # Relevant if GENERATE_SAMPLES is True
+TRAIN_PERCENT = 0.90  # Proportion of dataset used for training (rest used for test)
+TRAIN = True  # Train the model (if False, read from file)
+EPOCHS = 100  # Relefvant if TRAIN is True
+BATCH_SIZE = 1000  # Relevant if TRAIN is True
+SHOW_VOL_CHARTS = False  # Show strike ladder charts
+SAVE_MODEL = True  # Save model to files
 
 print(">> Set up runtime configuration")
 project_folder = os.path.join(settings.WORKFOLDER, "xsabr")
-print("> Project folder " + project_folder)
+print("> Project folder: " + project_folder)
 data_folder = os.path.join(project_folder, "samples")
-print("> Data folder " + data_folder)
+print("> Data folder: " + data_folder)
 check_directory(data_folder)
 print("> Chosen model: " + MODEL_TYPE)
 data_file = os.path.join(data_folder, MODEL_TYPE + "_samples.tsv")
+model_folder = os.path.join(project_folder, "models")
+print("> Model folder: " + model_folder)
+
+# ################ Helper functions ###############################################################
+def bps_rmse(y_true, y_ref):
+    """ RMSE in bps """
+    return 10000.0 * rmse(y_true, y_ref)
+
+def tf_bps_rmse(y_true, y_ref):
+    """ RMSE in bps in tensorflow """
+    return 10000.0 * tf_rmse(y_true, y_ref)
 
 # Generator factory
 if MODEL_TYPE == "SABR":
@@ -53,7 +66,8 @@ elif MODEL_TYPE == "ShiftedSABR":
 else:
     raise ValueError("Unknown model: " + MODEL_TYPE)
 
-# Generate dataset
+# ################ Training algorithm #############################################################
+# Generate dataset by prices and convert to normal vols
 if GENERATE_SAMPLES:
     print(f">> Generating {NUM_SAMPLES:,} samples")
     data_df = generator.generate_samples(NUM_SAMPLES)
@@ -63,7 +77,7 @@ if GENERATE_SAMPLES:
     generator.to_file(data_df, data_file)
 
 
-# If training, retrieve dataset, compose model and train
+# Retrieve dataset, compose and train the model on the normal vols
 if TRAIN:
     # Retrieve dataset
     print(">> Reading dataset from file: " + data_file)
@@ -102,16 +116,6 @@ if TRAIN:
     for field, value in optim_fields.items():
         print(field, ":", value)
 
-
-    # Customized loss (just scaling RMSE by 10,000)
-    def tf_bps_rmse(y_true, y_ref):
-        """ RMSE in bps in tensorflow """
-        return 10000.0 * tf_rmse(y_true, y_ref)
-
-    def bps_rmse(y_true, y_ref):
-        """ RMSE in bps """
-        return 10000.0 * rmse(y_true, y_ref)
-
     # Compile
     print("> Compile model")
     keras_model.compile(loss=tf_bps_rmse, optimizer=optimizer)
@@ -132,16 +136,28 @@ if TRAIN:
     trn_timer.stop()
     trn_timer.print()
 
+    # Check performance
+    train_pred = model.predict(x_train)
+    train_rmse = bps_rmse(train_pred, y_train)
+    print(f"RMSE on training set: {train_rmse:,.2f}")
+
+    test_pred = model.predict(x_test)
+    test_rmse = bps_rmse(test_pred, y_test)
+    print(f"RMSE on test set: {test_rmse:,.2f}")
+
+    # Save trained model to file
+    if SAVE_MODEL:
+        now = datetime.now()
+        dt_string = now.strftime("%Y%m%d-%H_%M_%S")
+        model_folder_name = os.path.join(model_folder, MODEL_TYPE + "_" + dt_string)
+        model.save(model_folder_name)
+
+else:  # Not training, so loading the model from file
+    print("Loading pre-trained model from files")
+
+# ################ Performance analysis ###########################################################
 # Analyse results
 print(">> Analyse results")
-
-print("> Performance on testing set")
-train_pred = model.predict(x_train)
-train_rmse = bps_rmse(train_pred, y_train)
-print(f"RMSE on training set: {train_rmse:,.2f}")
-test_pred = model.predict(x_test)
-test_rmse = bps_rmse(test_pred, y_test)
-print(f"RMSE on test set: {test_rmse:,.2f}")
 
 # Generate strike spread axis
 if SHOW_VOL_CHARTS:
@@ -173,26 +189,27 @@ if SHOW_VOL_CHARTS:
     plt.show()
 
 # Show training history
-hist_epochs = callback.epochs
-hist_losses = callback.losses
-hist_lr = callback.learning_rates
-sampled_epochs = callback.sampled_epochs
-test_losses = callback.test_losses
-# hist_epochs, hist_losses, hist_lr = callback.convergence()
+if TRAIN:
+    hist_epochs = callback.epochs
+    hist_losses = callback.losses
+    hist_lr = callback.learning_rates
+    sampled_epochs = callback.sampled_epochs
+    test_losses = callback.test_losses
+    # hist_epochs, hist_losses, hist_lr = callback.convergence()
 
-plt.figure(figsize=(14, 7))
-plt.subplots_adjust(hspace=0.40)
+    plt.figure(figsize=(14, 7))
+    plt.subplots_adjust(hspace=0.40)
 
-plt.subplot(1, 2, 1)
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.yscale("log")
-plt.plot(hist_epochs, hist_losses, label='Loss on training set')
-plt.plot(sampled_epochs, test_losses, color='red', label='Loss on test set')
-plt.legend(loc='upper right')
-plt.subplot(1, 2, 2)
-plt.xlabel('Epoch')
-plt.ylabel('Learning rate')
-plt.plot(hist_epochs, hist_lr)
+    plt.subplot(1, 2, 1)
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.yscale("log")
+    plt.plot(hist_epochs, hist_losses, label='Loss on training set')
+    plt.plot(sampled_epochs, test_losses, color='red', label='Loss on test set')
+    plt.legend(loc='upper right')
+    plt.subplot(1, 2, 2)
+    plt.xlabel('Epoch')
+    plt.ylabel('Learning rate')
+    plt.plot(hist_epochs, hist_lr)
 
-plt.show()
+    plt.show()
