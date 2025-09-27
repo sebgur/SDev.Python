a37b3b04642985d044e94d38735a22ffbd0e3c57
sebgur <sebgur@gmail.com>
2025-09-27 21:35:11 +0800

    up to Feed-Forward

--- sdevpy/llms/gpt.py
+++ sdevpy/llms/gpt.py
@@ -2,6 +2,42 @@ import torch
 import torch.nn as nn
 
 
+class FeedForward(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.layers = nn.Sequential(nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]),
+                                    GELU(),
+                                    nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"]),)
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(torch.sqrt(torch.tensor(2.0 / torch.pi)) *
+                                         (x + 0.044715 * torch.pow(x, 3))))
+
+
+class LayerNorm(nn.Module):
+    """ Normalize the input along its embedding direction, then scale it using trained scale and shift
+        parameters. """
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
+
+
 class DummyGPTModel(nn.Module):
     def __init__(self, cfg):
         super().__init__()
--- sdevpy/projects/raschka/ch4_gpt_model.py
+++ sdevpy/projects/raschka/ch4_gpt_model.py
@@ -1,10 +1,12 @@
-# from importlib.metadata import version
-# import torch
-# import numpy as np
-# from sdevpy.llms.attention import SelfAttentionV1, SelfAttentionV2, CausalAttention
-# from sdevpy.llms.attention import MultiHeadAttentionWrapper, MultiHeadAttention
+from importlib.metadata import version
+import torch
+import torch.nn as nn
+import tiktoken
+import matplotlib.pyplot as plt
+from sdevpy.llms.gpt import DummyGPTModel, LayerNorm, GELU, FeedForward
 
-# print("pytorch version: ", torch.__version__)
+print("tiktoken version:", version("tiktoken"))
+print("pytorch version: ", torch.__version__)
 
 
 GPT_CONFIG_124M = {
@@ -17,6 +19,77 @@ GPT_CONFIG_124M = {
     "qkv_bias": False       # Query-Key-Value bias
 }
 
-# print("<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>")
-# print("<><><><><><><><> Non-trainable self-attention <><><><><><><><><><><><><><><><>")
-# print("<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>\n")
+print("<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>")
+print("<><><><><><><><> Structure Overview <><><><><><><><><><><><><><><><><><><><><>")
+print("<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>\n")
+print("<><><><> Tokenize sample text <><><><>")
+tokenizer = tiktoken.get_encoding("gpt2")
+batch = []
+txt1 = "Every effort moves you"
+txt2 = "Every day holds a"
+batch.append(torch.tensor(tokenizer.encode(txt1)))
+batch.append(torch.tensor(tokenizer.encode(txt2)))
+batch = torch.stack(batch, dim=0)
+print("Batch\n", batch, "\n")
+
+print("Initialize GPT model")
+torch.manual_seed(123)
+model = DummyGPTModel(GPT_CONFIG_124M)
+logits = model(batch)
+print("Output shape: ", logits.shape)
+print("Logits\n", logits, "\n")
+
+print("<><><><> Layer Normalization <><><><>")
+torch.manual_seed(123)
+batch_example = torch.randn(2, 5)
+layer = nn.Sequential(nn.Linear(5, 6), nn.ReLU())
+out = layer(batch_example)
+print("Example batch\n", batch_example, "\n")
+print("NN output before normalization\n", out, "\n")
+mean = out.mean(dim=-1, keepdim=True)
+var = out.var(dim=-1, keepdim=True)
+print("Mean after normalization\n", mean, "\n")
+print("Variance after normalization\n", var, "\n")
+
+# Normalize
+print("<><><><> Normalize by simple formulas")
+torch.set_printoptions(sci_mode=False) # Turn off scientific notation
+out_norm = (out - mean) / torch.sqrt(var)
+mean = out_norm.mean(dim=-1, keepdim=True)
+var = out_norm.var(dim=-1, keepdim=True)
+print("Normalized\n", out_norm, "\n")
+print("Mean after normalization\n", mean, "\n")
+print("Variance after normalization\n", var, "\n")
+
+print("<><><><> Normalize using LayerNorm")
+ln = LayerNorm(emb_dim=5)
+out_ln = ln(batch_example)
+mean = out_ln.mean(dim=-1, keepdim=True)
+var = out_ln.var(dim=-1, keepdim=True, unbiased=False)
+print("Normalized\n", out_ln, "\n")
+print("Mean after LayerNorm\n", mean, "\n")
+print("Variance after LayerNorm\n", var, "\n")
+
+# print("<><><><> GELU")
+# gelu, relu = GELU(), nn.ReLU()
+# x = torch.linspace(-3, 3, 100)
+# y_gelu, y_relu = gelu(x), relu(x)
+# plt.figure(figsize=(8, 3))
+# for i, (y, label) in enumerate(zip([y_gelu, y_relu], ["GELU", "ReLU"]), 1):
+#     plt.subplot(1, 2, i)
+#     plt.plot(x, y)
+#     plt.title(f"{label} activation function")
+#     plt.xlabel("x")
+#     plt.ylabel(f"{label}(x)")
+#     plt.grid(True)
+# plt.tight_layout()
+# plt.show()
+
+print("<><><><> Feed-Forward")
+ffn = FeedForward(GPT_CONFIG_124M)
+x = torch.rand(2, 3, 768)
+out = ffn(x)
+print(f"Output shape: {out.shape}")
+
+
+
