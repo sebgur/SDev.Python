b9bd0a1d8fb8e8c79f6c9d43b74abf60bb8520b4
sebgur <sebgur@gmail.com>
2025-10-19 15:34:29 +0900

    adding preliminary for gpt2

---
+++ sdevpy/projects/chat_gpt2.py
@@ -0,0 +1,10 @@
+# Separate and streamline downloading of GPT2 family
+# Store all models in OneDrive
+# Develop chat script with end by end of text, period, max sentences, etc
+# Develop script for json instruction on raw GPT2 model
+# Develop script for pre-training on json instructions
+# Develop script for LORA fine-tuning on json instructions
+# Learn how to download/load Llama, Store Llama in OneDrive
+# Test Llama on json instruction
+# Test fine-tuning Llama on json instruction
+# Test code on GPU-enabled platforms
