1650b3cc87c49c982a62d963e3a27210f274ac1f
sebgur <sebgur@gmail.com>
2025-11-22 18:28:09 +0800

    trying more steps

--- sdevpy/pde/test_pde.py
+++ sdevpy/pde/test_pde.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.linalg import solve_banded
 from scipy.stats import norm
+import matplotlib.pyplot as plt
 from sdevpy.maths import tridiag
 
 ########## ToDo ###################################
@@ -28,10 +29,8 @@ def local_vol(t, x_grid):
     """ As a function of log forward moneyness """
     vol_atm = 0.20
     skew = -0.1
-    # vol = vol_atm + skew * x
-    # return np.maximum(0.01, vol)
-    # return np.asarray([vol_atm for x in x_grid])
-    return np.asarray([np.maximum(0.01, vol_atm + skew * x) for x in x_grid])
+    return np.asarray([vol_atm for x in x_grid])
+    # return np.asarray([np.maximum(0.01, vol_atm + skew * x) for x in x_grid])
 
 
 def build_timegrid(maturity, config):
@@ -58,10 +57,10 @@ def build_spotgrid(maturity, config):
 
 
 if __name__ == "__main__":
-    maturity = 2.5
-    time_config = {'n_steps': 2}
-    spot_config = {'n_meshes': 5, 'mesh_vol': 0.20, 'percentile': 1e-4}
-    scheme = {'theta': 0.5}
+    maturity = 5
+    time_config = {'n_steps': 100}
+    spot_config = {'n_meshes': 500, 'mesh_vol': 0.20, 'percentile': 1e-4}
+    scheme = {'theta': 1.0}
 
     ### Build time grid ####
     t_grid = build_timegrid(maturity, time_config)
@@ -78,6 +77,8 @@ if __name__ == "__main__":
     p = np.zeros(n_spotgrid)
     p[spot_idx] = 1.0
 
+    print(f"dx: {dx}")
+
     #### Backward reduction ####
     a = 1.0 / dx**2 + 0.5 / dx
     b = 2.0 / dx**2
@@ -88,33 +89,42 @@ if __name__ == "__main__":
         dt = te - ts
         theta = scheme['theta'] # Later this might depend on time (Rannacher)
         one_m_theta = 1.0 - theta
-        print(f"\nNew time: {te}")
+        # print(f"\nNew time: {te}")
 
         ## Calculate result vector using previous probabilities ##
         # Calculate local vol vector
         lv = local_vol(ts, x_grid)
-        print(f"Calculating LV at {ts}: {lv}")
+        # print(f"Calculating LV at {ts}: {lv}")
 
         # Calculate result vector
         one_m_theta_dt_2 = one_m_theta * dt / 2.0
         y = np.zeros(n_spotgrid)
         for j in range(n_spotgrid):
             p_tmp = (1.0 - one_m_theta_dt_2 * b * lv[j]**2) * p[j]
+            # if j == spot_idx:
+            #     print(p_tmp)
 
             if j < n_spotgrid - 1: # Beyond that the probability is 0
                 p_tmp += one_m_theta_dt_2 * a * lv[j + 1]**2 * p[j + 1]
 
+            # if j == spot_idx:
+            #     print(p_tmp)
+
             if j > 0: # Before that the probability is 0
                 p_tmp += one_m_theta_dt_2 * c * lv[j - 1]**2 * p[j - 1]
 
+            # if j == spot_idx:
+            #     print(p_tmp)
+
+
             y[j] = p_tmp
 
-        print(f"Calculating y: {y}")
+        # print(f"Calculating y: {y}")
 
         ## Calculate band vectors for tridiagonal system ##
         # Calculate local vol vector
         lv = local_vol(te, x_grid)
-        print(f"Calculating LV at {te}: {lv}")
+        # print(f"Calculating LV at {te}: {lv}")
         # Calculate bands
         theta_dt_2 = theta * dt / 2.0
         upper = np.zeros(n_spotgrid - 1)
@@ -131,4 +141,14 @@ if __name__ == "__main__":
 
         # Solve tridiagonal system
         x = tridiag.solve(upper, main, lower, y)
-        print(f"Calculating x: {x}")
+        # print(f"Calculating x: {x}")
+
+    x_g = []
+    p_g = []
+    for u, v in zip(x_grid, x):
+        if u > -0.25 and u < 0.25:
+            x_g.append(u)
+            p_g.append(v)
+
+    plt.plot(x_g, p_g)
+    plt.show()
