bc5472c4f9d4e9fe394fa4917527240917d4454c
sebgur <sebgur@gmail.com>
2025-11-23 17:03:50 +0800

    saving before defining forwardpde method

--- sdevpy/pde/forwardpde.py
+++ sdevpy/pde/forwardpde.py
@@ -7,8 +7,6 @@ from sdevpy.analytics import black
 from sdevpy.maths import metrics
 
 ########## ToDo ###################################
-# * Would make sense to wrap the PDE resolution forward into function that goes forward
-#   from a given ts to a given te, given the vector of probabilities calculated at ts.
 # * During the warmup in the time direction, we can allocate
 #   on each time slice a local vol functional form that is only a function
 #   of the spot. This would be a generalized version of the storage
@@ -65,11 +63,58 @@ def mollifier(x, x0, dx, k=1.5):
     # p /= np.trapezoid(p, x)
     return p
 
+
+def roll_forward(p, x, ts, te, local_vol, scheme):
+    """ Roll the density forward from time ts to te (ts < te) """
+    theta = scheme['theta']
+    dx = scheme['dx'] # Assuming homogeneous x grid
+    one_m_theta = 1.0 - theta
+    n_spotgrid = x.shape[0]
+    dt = te - ts
+    a = 1.0 / dx**2 + 0.5 / dx
+    b = 2.0 / dx**2
+    c = 1.0 / dx**2 - 0.5 / dx
+
+    # Calculate result vector using previous probabilities
+    lv = local_vol(ts, x)
+    one_m_theta_dt_2 = one_m_theta * dt / 2.0
+    y = np.zeros(n_spotgrid)
+    for j in range(n_spotgrid):
+        p_tmp = (1.0 - one_m_theta_dt_2 * b * lv[j]**2) * p[j]
+
+        if j < n_spotgrid - 1: # Beyond that the probability is 0
+            p_tmp += one_m_theta_dt_2 * a * lv[j + 1]**2 * p[j + 1]
+
+        if j > 0: # Before that the probability is 0
+            p_tmp += one_m_theta_dt_2 * c * lv[j - 1]**2 * p[j - 1]
+
+        y[j] = p_tmp
+
+    # Calculate band vectors for tridiagonal system
+    lv = local_vol(te, x)
+    theta_dt_2 = theta * dt / 2.0
+    upper = np.zeros(n_spotgrid - 1)
+    main = np.zeros(n_spotgrid)
+    lower = np.zeros(n_spotgrid - 1)
+    for j in range(n_spotgrid):
+        main[j] = (1.0 + theta_dt_2 * b * lv[j]**2)
+
+        if j < n_spotgrid - 1:
+            upper[j] = -theta_dt_2 * a * lv[j + 1]**2
+
+        if j > 0:
+            lower[j - 1] = -theta_dt_2 * c * lv[j - 1]**2
+
+    # Solve tridiagonal system
+    p_new = tridiag.solve(upper, main, lower, y)
+    return p_new
+
+
 if __name__ == "__main__":
     maturity = 2.5
     time_config = {'n_steps': 500}
     spot_config = {'n_meshes': 200, 'mesh_vol': atm_vol, 'percentile': 1e-6}
-    scheme = {'theta': 1.0}
+    scheme = {'theta': 1.0, 'mollifier': 1.5}
     print(f"Time steps: {time_config['n_steps']}")
     print(f"Spot steps: {spot_config['n_meshes']}")
 
@@ -80,63 +125,23 @@ if __name__ == "__main__":
 
     #### Build spot grid ####
     x_grid, dx, spot_idx = build_spotgrid(maturity, spot_config)
+    scheme['dx'] = dx
+    scheme['spot_idx'] = spot_idx
     # print(f"Spot grid:\n {x_grid}")
     # print(f"dx: {dx}")
 
     #### Initial probability ####
-    n_spotgrid = x_grid.shape[0]
-    p = np.zeros(n_spotgrid)
-    # Naive approach (Dirac)
-    # p[spot_idx] = 1.0
-    # Mollifier
-    p = mollifier(x_grid, 0.0, dx, 1.5)
+    p = mollifier(x_grid, 0.0, scheme['dx'], scheme['mollifier'])
 
     #### Backward reduction ####
-    a = 1.0 / dx**2 + 0.5 / dx
-    b = 2.0 / dx**2
-    c = 1.0 / dx**2 - 0.5 / dx
     for i in range(n_timegrid - 1):
         ts = t_grid[i]
         te = t_grid[i + 1]
-        dt = te - ts
-        theta = scheme['theta'] # Later this might depend on time (Rannacher)
-        one_m_theta = 1.0 - theta
-        # print(f"\nNew time: {te}")
 
-        ## Calculate result vector using previous probabilities ##
-        lv = local_vol(ts, x_grid)
-        one_m_theta_dt_2 = one_m_theta * dt / 2.0
-        y = np.zeros(n_spotgrid)
-        for j in range(n_spotgrid):
-            p_tmp = (1.0 - one_m_theta_dt_2 * b * lv[j]**2) * p[j]
+        # Roll forward from ts to te
+        p = roll_forward(p, x_grid, ts, te, local_vol, scheme)
 
-            if j < n_spotgrid - 1: # Beyond that the probability is 0
-                p_tmp += one_m_theta_dt_2 * a * lv[j + 1]**2 * p[j + 1]
-
-            if j > 0: # Before that the probability is 0
-                p_tmp += one_m_theta_dt_2 * c * lv[j - 1]**2 * p[j - 1]
-
-            y[j] = p_tmp
-
-        ## Calculate band vectors for tridiagonal system ##
-        lv = local_vol(te, x_grid)
-        theta_dt_2 = theta * dt / 2.0
-        upper = np.zeros(n_spotgrid - 1)
-        main = np.zeros(n_spotgrid)
-        lower = np.zeros(n_spotgrid - 1)
-        for j in range(n_spotgrid):
-            main[j] = (1.0 + theta_dt_2 * b * lv[j]**2)
-
-            if j < n_spotgrid - 1:
-                upper[j] = -theta_dt_2 * a * lv[j + 1]**2
-
-            if j > 0:
-                lower[j - 1] = -theta_dt_2 * c * lv[j - 1]**2
-
-        # Solve tridiagonal system
-        p = tridiag.solve(upper, main, lower, y)
-
-        # Check sum
+        # Check sum at te
         mass = np.trapezoid(p, x_grid)
         print(f"Mass: {mass:.6f}")
 
