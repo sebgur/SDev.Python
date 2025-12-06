e2d1a87d09202e1ccdd0ec64e389338079a1982b
sebgur <sebgur@gmail.com>
2025-12-06 16:54:22 +0800

    adding files(2)

---
+++ sdevpy/montecarlo/singlefactormc.py
@@ -0,0 +1,7 @@
+""" The simulation may be on double/time, but we need to retrieve the values
+of the spot and indexes on dates. We should make a convenient map for that.
+
+Would it be worth making it multi-factor from the start?
+
+Or is it better to simulate by date?
+"""
