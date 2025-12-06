3f554930d3f069c7b2b28901a3df1f5a41c999c3
sebgur <sebgur@gmail.com>
2025-12-06 10:33:40 +0800

    schemes

--- sdevpy/pde/forwardpde.py
+++ sdevpy/pde/forwardpde.py
@@ -5,8 +5,16 @@ import matplotlib.pyplot as plt
 from sdevpy.maths import tridiag
 from sdevpy.analytics import black
 from sdevpy.maths import metrics
+from sdevpy.pde import pdeschemes
 
-########## ToDo (basic) ###################################
+########## ToDo (basic) #######################################################
+# * Write explicit code for implicit scheme to be faster at runtime
+# * Write diagnostic that looks at several maturities in one flow
+# * Implement x grid rescaling at given times. Then do the rescaling at all
+#   the calibration times. Need to test that by extracting several densities
+#   out of the forward sweep and testing their accuracies across time.
+# * X grid rescaling to happen at first time in batch, this way no rescaling at
+#   end of previous batch. Mass rescaling could happen at the end.
 # * Analytical expression as additional scheme for early times. This is not really
 #   instead of the mollifier. Need to make good decision for where in time
 #   the PDE/roll-forward starts, basically. 1D is an obvious candidate. We could
@@ -15,11 +23,12 @@ from sdevpy.maths import metrics
 #   throw an error in fact, we could still do it just in case someone asks before
 #   that time. The use of the analytical should therefore be optional, to allow possibly someone to refuse
 #   to use it in order to go to earlier time steps.
-# * Rannacher for early times
-# * mass rescaling to 1, forward shifting
-# * Convergence tests along time and spot for different schemes and possibilities above
+# * Forward shifting?
 
-########## ToDo (calibration) ###################################
+########## ToDo (calibration) #################################################
+# * Add 1d solving to ATM only, to do live and Vega with smile solving less often.
+# * For each LV parametric form, record two sets of parameters: those against
+#   moneyness and those against strike, to achieve both stickiness.
 # * During the warmup in the time direction, we can allocate
 #   on each time slice a local vol functional form that is only a function
 #   of the spot. This would be a generalized version of the storage
@@ -41,18 +50,18 @@ from sdevpy.maths import metrics
 #   PDE as used in calibration, and then check against backward PDE.
 
 
-def forward_pde_density(maturity, local_vol, time_config, spot_config, scheme):
+def forward_pde_density(maturity, local_vol, time_config, spot_config, config):
     # Build time grid
     t_grid = build_timegrid(maturity, time_config)
     n_timegrid = t_grid.shape[0]
 
     # Build spot grid
     x, dx, spot_idx = build_spotgrid(maturity, spot_config)
-    scheme['dx'] = dx
-    scheme['spot_idx'] = spot_idx
+    config['dx'] = dx
+    config['spot_idx'] = spot_idx
 
     # Initial probability
-    p = mollifier(x, 0.0, scheme['dx'], scheme['mollifier'])
+    p = mollifier(x, 0.0, config['dx'], config['mollifier'])
 
     # Backward reduction
     for i in range(n_timegrid - 1):
@@ -60,7 +69,8 @@ def forward_pde_density(maturity, local_vol, time_config, spot_config, scheme):
         te = t_grid[i + 1]
 
         # Roll forward from ts to te
-        p = roll_forward(p, x, ts, te, local_vol, scheme)
+        p = roll_forward(p, x, ts, te, local_vol, config)
+        # p = roll_forward(p, x, ts, te, local_vol, scheme)
 
         # Check sum at te
         mass = np.trapezoid(p, x)
@@ -99,61 +109,70 @@ def mollifier(x, x0, dx, k=1.5):
     return p
 
 
-def roll_forward(p, x, ts, te, local_vol, scheme):
+def roll_forward(p, x, ts, te, local_vol, config):
     """ Roll the density forward from time ts to te (ts < te) """
-    theta = scheme['theta']
-    dx = scheme['dx'] # Assuming homogeneous x grid
-    one_m_theta = 1.0 - theta
-    n_x = x.shape[0]
-    dt = te - ts
-    a = 1.0 / dx**2 + 0.5 / dx
-    b = 2.0 / dx**2
-    c = 1.0 / dx**2 - 0.5 / dx
-
-    # Calculate result vector using previous probabilities
-    lv = local_vol(ts, x)
-    one_m_theta_dt_2 = one_m_theta * dt / 2.0
-    y = np.zeros(n_x)
-    for j in range(n_x):
-        p_tmp = (1.0 - one_m_theta_dt_2 * b * lv[j]**2) * p[j]
-
-        if j < n_x - 1: # Beyond that the probability is 0
-            p_tmp += one_m_theta_dt_2 * a * lv[j + 1]**2 * p[j + 1]
-
-        if j > 0: # Before that the probability is 0
-            p_tmp += one_m_theta_dt_2 * c * lv[j - 1]**2 * p[j - 1]
-
-        y[j] = p_tmp
-
-    # Calculate band vectors for tridiagonal system
-    lv = local_vol(te, x)
-    theta_dt_2 = theta * dt / 2.0
-    upper = np.zeros(n_x - 1)
-    main = np.zeros(n_x)
-    lower = np.zeros(n_x - 1)
-    for j in range(n_x):
-        main[j] = (1.0 + theta_dt_2 * b * lv[j]**2)
-
-        if j < n_x - 1:
-            upper[j] = -theta_dt_2 * a * lv[j + 1]**2
-
-        if j > 0:
-            lower[j - 1] = -theta_dt_2 * c * lv[j - 1]**2
-
-    # Solve tridiagonal system
-    p_new = tridiag.solve(upper, main, lower, y)
-
-    # ToDo: Rescale/recenter here if required?
-
+    dx = config['dx'] # Assuming homogeneous x grid
+    scheme = pdeschemes.scheme(config, ts)
+    scheme.local_vol = local_vol
+    p_new = scheme.roll_forward(p, x, ts, te, dx)
     return p_new
+
+
+# def roll_forward(p, x, ts, te, local_vol, scheme):
+#     """ Roll the density forward from time ts to te (ts < te) """
+#     theta = scheme['theta']
+#     dx = scheme['dx'] # Assuming homogeneous x grid
+#     one_m_theta = 1.0 - theta
+#     n_x = x.shape[0]
+#     dt = te - ts
+#     a = 1.0 / dx**2 + 0.5 / dx
+#     b = 2.0 / dx**2
+#     c = 1.0 / dx**2 - 0.5 / dx
+
+#     # Calculate result vector using previous probabilities
+#     lv = local_vol(ts, x)
+#     one_m_theta_dt_2 = one_m_theta * dt / 2.0
+#     y = np.zeros(n_x)
+#     for j in range(n_x):
+#         p_tmp = (1.0 - one_m_theta_dt_2 * b * lv[j]**2) * p[j]
+
+#         if j < n_x - 1: # Beyond that the probability is 0
+#             p_tmp += one_m_theta_dt_2 * a * lv[j + 1]**2 * p[j + 1]
+
+#         if j > 0: # Before that the probability is 0
+#             p_tmp += one_m_theta_dt_2 * c * lv[j - 1]**2 * p[j - 1]
+
+#         y[j] = p_tmp
+
+#     # Calculate band vectors for tridiagonal system
+#     lv = local_vol(te, x)
+#     theta_dt_2 = theta * dt / 2.0
+#     upper = np.zeros(n_x - 1)
+#     main = np.zeros(n_x)
+#     lower = np.zeros(n_x - 1)
+#     for j in range(n_x):
+#         main[j] = (1.0 + theta_dt_2 * b * lv[j]**2)
+
+#         if j < n_x - 1:
+#             upper[j] = -theta_dt_2 * a * lv[j + 1]**2
+
+#         if j > 0:
+#             lower[j - 1] = -theta_dt_2 * c * lv[j - 1]**2
+
+#     # Solve tridiagonal system
+#     p_new = tridiag.solve(upper, main, lower, y)
+
+#     # ToDo: Rescale/recenter here if required?
+
+#     return p_new
 
 
 if __name__ == "__main__":
     maturity = 2.5
     spot = 100.0
-    r = 0.0
-    q = 0.0
-    r_disc = 0.0
+    r = 0.04
+    q = 0.01
+    r_disc = 0.025
     fwd = spot * np.exp((r - q) * maturity)
     atm_vol = 0.20
 
@@ -167,12 +186,12 @@ if __name__ == "__main__":
     #### Diagnostics (single run) #################################################################
     time_config = {'n_steps': 500}
     spot_config = {'n_meshes': 200, 'mesh_vol': atm_vol, 'percentile': 1e-6}
-    scheme = {'theta': 1.0, 'mollifier': 1.5}
+    scheme_config = {'mollifier': 1.5, 'type': 'Rannacher', 'theta': 1.0, 'rannacher_time': 0.1}
     print(f"Time steps: {time_config['n_steps']}")
     print(f"Spot steps: {spot_config['n_meshes']}")
 
     # Calculate probability density
-    x, p = forward_pde_density(maturity, my_lv, time_config, spot_config, scheme)
+    x, p = forward_pde_density(maturity, my_lv, time_config, spot_config, scheme_config)
 
     ## Check density ##
     # Range
@@ -204,6 +223,7 @@ if __name__ == "__main__":
     plt.plot(pde_x, pde_p, label="PDE", color='red')
     plt.plot(cf_x, cf_p, label="CF", color='blue')
     plt.legend()
+    plt.title("Density, PDE vs CF")
     plt.show()
 
     ## Check option prices ##
@@ -229,6 +249,7 @@ if __name__ == "__main__":
     plt.plot(strikes, pde_prices - it_prices, label="PDE", color='red')
     plt.plot(strikes, cf_prices - it_prices, label="CF", color='blue')
     plt.legend()
+    plt.title("Option prices, PDE vs CF")
     plt.show()
 
     #### Diagnostics (convergence) ################################################################
---
+++ sdevpy/pde/pdeschemes.py
@@ -0,0 +1,144 @@
+import numpy as np
+from abc import ABC, abstractmethod
+from sdevpy.maths import tridiag
+
+
+def scheme(config, ts):
+    scheme_type = config['type'].upper()
+    if scheme_type == 'IMPLICIT':
+        return ImplicitScheme()
+    elif scheme_type == 'CN':
+        return ThetaScheme(0.5)
+    elif scheme_type == 'THETA':
+        return ThetaScheme(config['theta'])
+    elif scheme_type == 'EXPLICIT':
+        return ExplicitScheme()
+    elif scheme_type == 'RANNACHER':
+        rt = config['rannacher_time']
+        eps = 1e-8
+        if rt < ts - eps:
+            return ImplicitScheme()
+        else:
+            return ThetaScheme(0.5)
+    else:
+        raise TypeError(f"Unknown PDE scheme type: {scheme_type}")
+
+
+class PdeScheme(ABC):
+    def __init__(self):
+        self.local_vol = None
+
+    @abstractmethod
+    def roll_forward(self, p, x, ts, te, dx):
+        pass
+
+    # def set_local_vol(self, local_vol):
+    #     self.local_vol = local_vol
+
+
+class ThetaScheme(PdeScheme):
+    """ Mixing scheme with particular sub-cases:
+            Theta = 0.0: Implicit
+            Theta = 0.5: Crank-Nicolson
+            Theta = 1.0: Explicit """
+    def __init__(self, theta):
+        super().__init__()
+        self.theta = theta
+        self.one_m_theta = 1.0 - theta
+
+    def roll_forward(self, p, x, ts, te, dx):
+        n_x = x.shape[0]
+        dt = te - ts
+        a = 1.0 / dx**2 + 0.5 / dx
+        b = 2.0 / dx**2
+        c = 1.0 / dx**2 - 0.5 / dx
+
+        # Calculate result vector using previous probabilities
+        lv = self.local_vol(ts, x)
+        one_m_theta_dt_2 = self.one_m_theta * dt / 2.0
+        y = np.zeros(n_x)
+        for j in range(n_x):
+            p_tmp = (1.0 - one_m_theta_dt_2 * b * lv[j]**2) * p[j]
+
+            if j < n_x - 1: # Beyond that the probability is 0
+                p_tmp += one_m_theta_dt_2 * a * lv[j + 1]**2 * p[j + 1]
+
+            if j > 0: # Before that the probability is 0
+                p_tmp += one_m_theta_dt_2 * c * lv[j - 1]**2 * p[j - 1]
+
+            y[j] = p_tmp
+
+        # Calculate band vectors for tridiagonal system
+        lv = self.local_vol(te, x)
+        theta_dt_2 = self.theta * dt / 2.0
+        upper = np.zeros(n_x - 1)
+        main = np.zeros(n_x)
+        lower = np.zeros(n_x - 1)
+        for j in range(n_x):
+            main[j] = (1.0 + theta_dt_2 * b * lv[j]**2)
+
+            if j < n_x - 1:
+                upper[j] = -theta_dt_2 * a * lv[j + 1]**2
+
+            if j > 0:
+                lower[j - 1] = -theta_dt_2 * c * lv[j - 1]**2
+
+        # Solve tridiagonal system
+        p_new = tridiag.solve(upper, main, lower, y)
+        return p_new
+
+
+class ImplicitScheme(PdeScheme):
+    def __init__(self):
+        super().__init__()
+
+    def roll_forward(self, p, x, ts, te, dx):
+        n_x = x.shape[0]
+        dt = te - ts
+        a = dt / 2.0 * (1.0 / dx**2 + 0.5 / dx)
+        b = dt / dx**2
+        c = dt / 2.0 * (1.0 / dx**2 - 0.5 / dx)
+
+        lv = self.local_vol(te, x)
+        upper = np.zeros(n_x - 1)
+        main = np.zeros(n_x)
+        lower = np.zeros(n_x - 1)
+        for j in range(n_x):
+            main[j] = (1.0 + b * lv[j]**2)
+
+            if j < n_x - 1:
+                upper[j] = -a * lv[j + 1]**2
+
+            if j > 0:
+                lower[j - 1] = -c * lv[j - 1]**2
+
+        # Solve tridiagonal system
+        p_new = tridiag.solve(upper, main, lower, p)
+        return p_new
+
+
+class ExplicitScheme(PdeScheme):
+    def __init__(self):
+        super().__init__()
+
+    def roll_forward(self, p, x, ts, te, dx):
+        n_x = x.shape[0]
+        dt = te - ts
+        a = dt / 2.0 * (1.0 / dx**2 + 0.5 / dx)
+        b = dt / dx**2
+        c = dt / 2.0 * (1.0 / dx**2 - 0.5 / dx)
+
+        lv = self.local_vol(ts, x)
+        y = np.zeros(n_x)
+        for j in range(n_x):
+            p_tmp = (1.0 - b * lv[j]**2) * p[j]
+
+            if j < n_x - 1: # Beyond that the probability is 0
+                p_tmp += a * lv[j + 1]**2 * p[j + 1]
+
+            if j > 0: # Before that the probability is 0
+                p_tmp += c * lv[j - 1]**2 * p[j - 1]
+
+            y[j] = p_tmp
+
+        return y
