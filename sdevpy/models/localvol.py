55afa14081ae320fccebd66488045f45d7f4cb3d
sebgur <sebgur@gmail.com>
2025-12-13 17:10:01 +0800

    adding time grid lookup for LV

--- sdevpy/models/localvol.py
+++ sdevpy/models/localvol.py
@@ -1,18 +1,15 @@
 from abc import ABC, abstractmethod
 from sdevpy.models.svi import *
+from sdevpy.tools import algos
 
-########## ToDo (LocalVol) #################################################
-# * Implement/Finish testing the time lookup with tolerance
-# * Include it in the definition of the localVol
-# * Initialize LV
+
+########## ToDo (calibration) #################################################
 # * Create objective function, constraints, with pre-calculation of weighted
 #   payoff that doesn't depend on vols, define it as a generic payoff in a
 #   payoff class.
 # * Define the payoff class's core algebraic operations for Add, Multiply, etc.
 # * Refresh optimizer implementation, get definition/control of stopping
 #   criteria.
-
-########## ToDo (calibration) #################################################
 # * Implement calibration by sections
 # * Use seaborn to represent diffs between IV and LV prices on quoted pillars
 # * Add 1d solving to ATM only, to do live and Vega with smile solving less often.
@@ -35,13 +32,12 @@ from sdevpy.models.svi import *
 class LocalVol(ABC):
     """ Base class for local vols """
     @abstractmethod
-    def value(self, t, x):
-        """ Where x is the log-moneyness """
+    def value(self, t, logm):
         pass
 
     @abstractmethod
     def section(self, t):
-        """ Get a section at time t, i.e. a function of the log-moneyness x"""
+        """ Get a section at time t, i.e. a function of the log-moneyness log_m"""
         pass
 
 
@@ -53,8 +49,9 @@ class InterpolatedParamLocalVol(LocalVol):
         if len(section_grid) != t_grid.shape[0]:
             raise RuntimeError("Incorrect sizes between time grid and section grid")
 
-    def value(self, t, x):
-        return 0
+    def value(self, t, logm):
+        t_idx = algos.upper_bound(t_grid, t)
+        return section_grid[t_idx].value(t, logm)
 
     def section(self, t):
         return 0
@@ -68,6 +65,22 @@ class InterpolatedParamLocalVol(LocalVol):
 
 if __name__ == "__main__":
     t_grid = np.array([0.1, 0.25, 0.5, 1.0, 2.0, 5.0])
+    alnv = 0.25
+    a = alnv**2 * np.sqrt(1.0) # a > 0
+    b = 0.2 # b > 0
+    rho = 0.0 # -1 < rho < 1
+    m = 0.5 # No constraints
+    sigma = 0.25 # > 0
+    params = [a, b, rho, m, sigma]
+
     section_grid = [SviSection() for i in range(t_grid.shape[0])]
     lv = InterpolatedParamLocalVol(t_grid, section_grid)
-    print("Hello")
+
+    # Initialize parameters
+    for t_idx in range(len(t_grid)):
+        lv.update_params(t_idx, params)
+
+    lv_t = 0.75
+    lv_logm = 0.0
+    lvol = lv.value(lv_t, lv_logm)
+    print(f"T/LOG-M/LV: {lv_t}/{lv_logm}/{lvol}")
