90a3ca973a1ededd56de8cf517cc99a5b3ae7043
sebgur <sebgur@gmail.com>
2025-12-20 14:12:36 +0800

    examples of groupby and pivot_table

--- sdevpy/test.py
+++ sdevpy/test.py
@@ -6,3 +6,23 @@ import matplotlib.pyplot as plt
 # import json
 
 
+df = pd.DataFrame({
+    'store': ['A', 'A', 'B', 'B', 'A', 'B'],
+    'product': ['apple', 'banana', 'apple', 'banana', 'apple', 'banana'],
+    'sales': [10, 15, 20, 18, 12, 16],
+    'cost': [1, 1.5, 2, 1.8, 1.2, 1.6],
+})
+
+print(df)
+
+m = df.groupby('store')[['sales', 'cost']].sum()
+print(m)
+# Output: store A: 123.33, store B: 180.0
+
+# Group by multiple columns
+x = df.groupby(['store', 'product'])[['sales', 'cost']].sum()
+print(x)
+x.to_csv("C:\\temp\\pivot.csv")
+
+p = df.pivot_table(values='sales', index='store', columns='product', aggfunc='sum')
+print(p)
