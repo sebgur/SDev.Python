fixing overflow

examples/pde/ex_forwardpde.py
@@ -55,7 +55,7 @@ print(f"LV time grid: {np.asarray(lv.t_grid)}")
 print("Define PDE config")
 mesh_vol = calib_vols[0]
 pde_config = PdeConfig(n_timesteps=50, n_meshes=250, mesh_vol=mesh_vol, scheme='rannacher',
-                        rescale_x=True, rescale_p=True, iv_surface=iv_surface)
+                        rescale_x=True, rescale_p=True)
 print(f"Time steps: {pde_config.n_timesteps}")
 print(f"Spot steps: {pde_config.n_meshes}")
 print(f"Mesh vol: {mesh_vol*100:.2f}%")

sdevpy/volatility/impliedvol/models/biexp.py
@@ -91,15 +91,17 @@ def biexp(x, *params):
     if isequal(taur, 0.0):
         raise ValueError("taur=0 is not an acceptable parameter")
 
+    xm_r = np.maximum(xm, 0.0)
     ar = fp + (f0 - fr) / taur
-    volr = fr + (ar * xm + f0 - fr) * np.exp(-xm / taur)
+    volr = fr + (ar * xm_r + f0 - fr) * np.exp(-xm_r / taur)
 
     # Left case
     if isequal(taur, 0.0):
         raise ValueError("taul=0 is not an acceptable parameter")
 
+    xm_l = np.minimum(xm, 0.0)
     al = fp - (f0 - fl) / taul
-    voll = fl + (al * xm + f0 - fl) * np.exp(xm / taul)
+    voll = fl + (al * xm_l + f0 - fl) * np.exp(xm_l / taul)
 
     return np.where(xm >= 0.0, volr, voll)
 