e49746793c6db8a97a8e0e183e417887c61d8d52
sebgur <sebgur@gmail.com>
2026-09-21 23:10:51 +0800

    saving comments

--- sdevpy/volatility/fx/fx_volcalib.py
+++ sdevpy/volatility/fx/fx_volcalib.py
@@ -20,9 +20,9 @@ log = logging.getLogger(__name__)
 ################## TODO ###########################################################################
 # * Fix past Claude review, iterate a few times until relatively clean.
 # * Input real data, see if it resolves arbitrage checks
-# * Ask Codex for entire analysis of calibration flow. Iterate and converge.
 # * Check if bad vanna-volga extra points still happen at 1M
 # * Ask Codex about the bad vanna-volga extra points
+# * Ask Codex for entire analysis of calibration flow. Check again with Opus.
 # * Move yieldcurves to calib data provider
 # * Document solution search and market strangle in latex, add explanations in code comments
 
